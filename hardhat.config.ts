0e51c5e89c087c138bfcd4ac432794a0f510c97d
alvannwanorim <alvannwanorim@gmail.com>
2023-12-29 17:32:57 +0100

    Feat: basic NFT implementation

---
+++ hardhat.config.ts
@@ -0,0 +1,19 @@
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import { HardhatUserConfig } from "hardhat/config"
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  namedAccounts: {
+    deployer: {
+      default: 0
+    }
+  }
+};
+
+export default config;
---
+++ utils/verify.ts
@@ -0,0 +1,19 @@
+import {run} from "hardhat"
+const verify = async function (constractAddress: string, args: any[]) {
+    console.log("verifying contract")
+    try {
+        await run("verify:verifiy",{
+            address: constractAddress,
+            constructorArguments: args
+        })
+    } catch (e:any) {
+        if(e.message.toLowerCase().includes("already verified")){
+            console.log("already verified");
+            
+        }else{
+            console.log(e)
+        }
+    }
+}
+
+export default verify
